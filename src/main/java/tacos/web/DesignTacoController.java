062fab1e53a71d54f88920b16ac6d347b3d59f68
Thanos <thanasisvlassis@hotmail.com>
2021-02-25 16:18:20 +0200

    POST dummy method

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
@@ -43,6 +44,17 @@ public class DesignTacoController {
 
         return "design";
     }
+
+
+    @PostMapping
+    public String processDesign (Taco design) {
+//        save the taco design
+//        we'll do this on chapter 3
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
+    }
+
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
 
